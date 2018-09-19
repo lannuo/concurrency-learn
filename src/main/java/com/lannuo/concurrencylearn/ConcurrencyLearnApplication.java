d62fd3b11e7dad8d57b87c8cf9b75a5ace5b31f3
lannuo <lannuo5401@gmail.com>
2018-09-19 10:21:58 +0800

    init

---
+++ src/main/java/com/lannuo/concurrencylearn/ConcurrencyLearnApplication.java
@@ -0,0 +1,12 @@
+package com.lannuo.concurrencylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyLearnApplication.class, args);
+    }
+}
